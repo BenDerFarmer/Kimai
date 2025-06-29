6c17f6bbedace26de26afd35502580611eb93cb2
BenDerFarmer <chaoten@neverstopgaming.net>
2025-06-29 15:59:59 +0200

    feat: projects page show sperator for finished projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,33 +1,54 @@
 import { createSignal, onMount } from "solid-js";
 import { Kimai } from "../kimai";
 import { OptionsIcon } from "../components/Icons";
+import { project } from "../lib/timesheet";
 
 export function Projects() {
   const [projects, setProjects] = createSignal([]);
 
   onMount(async () => {
-    setProjects(await Kimai.getProjects({ ignoreDates: 1 }));
+    const project = await Kimai.getProjects({ ignoreDates: 1 });
+    project.sort((a, b) => {
+      if (b.end == undefined) {
+        return 1;
+      }
+      if (a.end == undefined) {
+        return -1;
+      }
+
+      return 0;
+    });
+    setProjects(project);
   });
 
   return (
     <ul class="list bg-base-100 rounded-box shadow-md pb-16">
       <For each={projects()}>
-        {(project, _) => (
-          <li class="list-row flex justify-between items-center ">
-            <div>
-              <div class={project.end != null ? "text-warning" : ""}>
-                {project.name}
+        {(project, i) => (
+          <>
+            <Show
+              when={
+                project.end != undefined && projects()[i() - 1].end == undefined
+              }
+            >
+              <li class="bg-base-300 p-4 pb-2 text-xs opacity-60 tracking-wide">
+                Beendete Projekte
+              </li>
+            </Show>
+            <li class="list-row flex justify-between items-center ">
+              <div>
+                <div> {project.name}</div>
+                <div class="text-xs uppercase font-semibold opacity-60">
+                  {project.parentTitle}
+                </div>
               </div>
-              <div class="text-xs uppercase font-semibold opacity-60">
-                {project.parentTitle}
+              <div class="flex">
+                <button class="btn btn-square btn-ghost">
+                  <OptionsIcon />
+                </button>
               </div>
-            </div>
-            <div class="flex">
-              <button class="btn btn-square btn-ghost">
-                <OptionsIcon />
-              </button>
-            </div>
-          </li>
+            </li>
+          </>
         )}
       </For>
     </ul>
